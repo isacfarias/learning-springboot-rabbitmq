e7f11ce2d9affbdc0fbb2840a2033551ae1fd84f
isacfarias <isac.farias.21@gmail.com>
2021-03-22 20:36:33 -0400

    first commit

---
+++ consomer-api/src/main/java/com/farias/consomerapi/ConsomerApiApplication.java
@@ -0,0 +1,13 @@
+package com.farias.consomerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsomerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsomerApiApplication.class, args);
+	}
+
+}
---
+++ consomer-api/src/test/java/com/farias/consomerapi/ConsomerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farias.consomerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsomerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
